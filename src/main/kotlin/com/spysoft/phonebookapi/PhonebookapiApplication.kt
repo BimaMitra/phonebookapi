a8f150fddb867d27cedbfc5a4ac1d017584d4cd3
Deepak Jalan <djalan@spysoft.com>
2019-10-24 10:19:03 +0530

    Initial Commit

---
+++ src/main/kotlin/com/spysoft/phonebookapi/PhonebookapiApplication.kt
@@ -0,0 +1,11 @@
+package com.spysoft.phonebookapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhonebookapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhonebookapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/spysoft/phonebookapi/PhonebookapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spysoft.phonebookapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhonebookapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
